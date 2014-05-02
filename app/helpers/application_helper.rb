7b607b3db23cdac663592ffa2f03a75cb4713399
Jesse McGinnis <jessieelsom@hotmail.com>
2014-05-02 00:54:59 -0400

    Almost done 'template' for sub-pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-  def nav_link(link_text, link_path, html_options={})
-    class_name = if current_page?(link_path) then 'active' else '' end
-
-    content_tag(:li, class: class_name) do
-      link_to link_text, link_path, html_options
-    end
-  end
-
 end
