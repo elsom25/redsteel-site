38bb86919bdd3663a090e5406a0e43655cc84c86
Jesse McGinnis <jessieelsom@hotmail.com>
2014-05-09 17:20:26 -0400

    Added rubocop and hound config files.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
-
   def nav_link(link_text, link_path, html_options={})
-    class_name = if current_page?(link_path) then 'active' else '' end
+    class_name = current_page?(link_path) ? 'active' : ''
 
     content_tag(:li, class: class_name) do
       link_to link_text, link_path, html_options
     end
   end
-
 end
