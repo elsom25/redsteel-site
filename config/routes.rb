9c35349af8dd8cd5f6f77b657124366a359e4af6
Jesse McGinnis <jessieelsom@hotmail.com>
2014-05-19 15:47:10 -0400

    Removing signup page on /about

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ RedSteelSite::Application.routes.draw do
   postmarkdown as: :blog
 
   get '/about' => 'pages#about', as: 'about'
-  post '/about' => 'pages#about_create', as: 'about_registration'
-
   get '/team' => 'pages#team', as: 'team'
 
   post '/' => 'registration#create', as: 'registration'
