1a91ffb1002f4a51732fc3ddb378a6b6ace7bca3
Jesse McGinnis <jessieelsom@hotmail.com>
2014-05-19 15:38:54 -0400

    Add a registration form on the about page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,25 @@
 class PagesController < ApplicationController
   layout 'sub_page'
+
+  def about
+    @registration = Registration.new
+  end
+
+  def about_create
+    @registration = Registration.new(registration_params)
+
+    if @registration.subscribe
+      cookies[:subscribed] = true
+      redirect_to about_path, notice: @registration.notice
+    else
+      flash.now[:error] = @registration.error_message
+      render action: :about
+    end
+  end
+
+protected
+
+  def registration_params
+    params[:registration]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ RedSteelSite::Application.routes.draw do
   postmarkdown as: :blog
 
   get '/about' => 'pages#about', as: 'about'
+  post '/about' => 'pages#about_create', as: 'about_registration'
+
   get '/team' => 'pages#team', as: 'team'
 
   post '/' => 'registration#create', as: 'registration'
